24634010e6c3a08ce7663924bc0d71c336a7da4c
LuanEscritorio <luanlima747@gmail.com>
2025-01-14 14:20:33 -0300

    adicionado construtores, getters e setters, e validadores nos metodos setters.

--- src/entities/Employee.java
+++ src/entities/Employee.java
@@ -1,4 +1,35 @@
 package entities;
 
 public class Employee {
+    private String name;
+    private Double salary;
+
+    public Employee(){}
+
+    public Employee(String name, Double salary){
+        this.name = name;
+        this.salary = salary;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        if(name == null || name.trim().isEmpty()){
+            throw new IllegalArgumentException("Name Can't be null or empty ");
+        }
+        this.name = name;
+    }
+
+    public Double getSalary() {
+        return salary;
+    }
+
+    public void setSalary(Double salary) {
+        if(salary == null || salary <= 0){
+            throw new IllegalArgumentException(("Salary Can't be Null or less than zero"));
+        }
+        this.salary = salary;
+    }
 }
